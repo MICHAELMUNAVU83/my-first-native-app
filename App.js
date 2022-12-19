775b25e9d507acc6ae3e46de705af1a975797716
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-19 14:53:12 +0300

    started app

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  Alert,
+  Image,
+  TouchableHighlight,
+} from "react-native";
+
 
 export default function App() {
+  const handlePress = () => alert("Text pressed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>
+        Open up App.js to start working on yourygbnhm app!yjkl;6ghjhjklojik
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            blurRadius: 2,
+
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="Click me"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            {
+              text: "Yes",
+              onPress: () => console.log("Yes"),
+            },
+            {
+              text: "No",
+              onPress: () => console.log("No"),
+            },
+          ])
+        }
+      />
+
+      <Button
+        color="orange"
+        title="Click me"
+        onPress={() =>
+          alert("hi")
+        }
+      />
+       
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +62,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
